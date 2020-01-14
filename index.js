4e527618074bf207cb0cec13d40fc9191c6d1837
Michael Kopsho <michael.kopsho@gmail.com>
2020-01-14 01:24:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  string.toLowerCase === string
+  if (string.toLowerCase === string) {
+    return "I can't hear you!";
+  else if (string.toUpperCase === string) {
+    return "YES INDEED!"
+  }
+  }
 }
